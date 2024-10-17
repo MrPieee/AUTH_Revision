dff29736ad50cec10cf1baaa98e56e1d78a7239a
Abu Sayed <abus04651@gmail.com>
2024-10-17 11:03:03 +0600

    medium level  authentication system

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app =express();
 const normalAuth = require('./auth1/normalAuth.route');
 const hashAuth = require('./auth2/hashPass.route');
 const bcryptAuth = require('./auth3/bcryptPass.route');
+const USER = require('./model/user.model');
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -17,6 +18,35 @@ app.get('/',(req,res)=>{
         return res.status(200).json({success:true, message: 'Welcome to server authentication revision project'});
     } catch (error) {
         return res.status(500).json(`Something broke : ${error.message}`);
+    };
+});
+
+// Get users from DB
+app.get('/users',async(req,res)=>{
+    try {
+        const users= await USER.find();
+        if(users.length>0){
+            return res.status(200).json(users);
+        }else{
+            return res.status(404).json({success:false,message:`Data not found`});
+        }
+    } catch (error) {
+        return res.status(500).json({success:false,message:`Something broke :${error.message}`});
+    };
+});
+
+
+// Get single user from DB
+app.get('/user/:id',async(req,res)=>{
+    try {
+        const user = await USER.findOne({_id:req.params.id});
+        if(user==!null){
+            return res.status(200).json(user);
+        }else{
+            return res.status(404).json({success:false,message:`Data not found`});
+        }
+    } catch (error) {
+        return res.status(500).json({success:false,message:`Something broke :${error.message}`});
     };
 });
 
--- auth2/hashPass.route.js
+++ auth2/hashPass.route.js
@@ -1,12 +1,48 @@
+const USER = require('../model/user.model');
+const md5 = require('md5');
 const router =require('express').Router();
 
 
+router.post('/register',async(req,res)=>{
+    try {
+        const {name ,username,password}=req.body;
+        if(name && username && password){
+            const hashedPass = md5(password);
+            const addUser =await USER({
+                name:name,
+                username:username,
+                password:hashedPass
+            });
+            await addUser.save();
+            return res.status(201).json({success:true,message:'User created',addUser});S
+        }else{
+            return res.status(400).json({success:false,message:`Please fill the form`});
+        }
+    } catch (error) {
+        return res.status(500).json({success:false,message:`Something broke :${error.message}`});
+    };
+});
 
 
 
-
-
-
+router.post('/logIn',async(req,res)=>{
+    try {
+        const {username,password}=req.body;
+        if( username && password){
+            const user = await USER.findOne({username:username});
+            const hashedPass = md5(password);
+            if(user && user.password===hashedPass){
+                return res.status(200).json({success:true, message :"user Login Successfully"});
+            }else{
+                return res.status(404).json({success:false,message:`Authentication Failed`});
+            }
+        }else{
+            return res.status(400).json({success:false,message:`Please fill the form`});
+        }
+    } catch (error) {
+        return res.status(500).json({success:false,message:`Something broke :${error.message}`});
+    };
+});
 
 
 module.exports = router;
